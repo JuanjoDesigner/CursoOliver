15e8f30b1cb9d674e2d3d155ab7da724db0094d4
JuanjoDesigner <juanjodesigner@gmail.com>
2014-02-03 23:32:57 +0100

    Controles3

--- android-controles3/src/com/jdesigner/android/controles3/MainActivity.java
+++ android-controles3/src/com/jdesigner/android/controles3/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -13,24 +16,47 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//Capturamos los checkbox y escribimos estado
+		//Capturamos los checkbox1 y programamos el marcado/desmarcado
 		final CheckBox checkBox1 = (CheckBox)findViewById(R.id.checkBox1);
-		final CheckBox checkBox2 = (CheckBox)findViewById(R.id.checkBox2);
-		final TextView txtEstado = (TextView)findViewById(R.id.txtEstado);
+		checkBox1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked) {
+					checkBox1.setText("Check marcado");
+				} else {
+					checkBox1.setText("Check desmarcado");
+				}
+				
+			}
+		});
 		
-		if(checkBox1.isChecked()) {
-			if(checkBox2.isChecked()) {
-				txtEstado.setText("Ambos check marcados");
-			} else {
-				txtEstado.setText("Primer check marcado");
+		//Capturamos los checkbox2 y programamos el marcado/desmarcado
+		final CheckBox checkBox2 = (CheckBox)findViewById(R.id.checkBox2);
+		checkBox2.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked) {
+					checkBox2.setText("Check marcado");
+				} else {
+					checkBox2.setText("Check desmarcado");
+				}
+				
 			}
-		} else {
-			if(checkBox2.isChecked()) {
-				txtEstado.setText("Segundo check marcado");
-			} else {
-				txtEstado.setText("Ningún Check marcado");
+		});
+		
+		//Capturamos el radiobutton y programamos el marcado del radiobutton
+		final TextView textradiobutton = (TextView)findViewById(R.id.textRadioButton);
+		final RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
+		rg.clearCheck();
+		rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				textradiobutton.setText("RadioButton marcado en posición " + checkedId+1);
 			}
-		}
+		});
 	}
 
 	@Override
