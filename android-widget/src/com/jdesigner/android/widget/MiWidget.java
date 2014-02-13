2c8b56df3fa9889eb9a935b83a5dfa4f5f93fc7c
JuanjoDesigner <juanjodesigner@gmail.com>
2014-02-13 14:01:13 +0100

    Widgets

---
+++ android-widget/src/com/jdesigner/android/widget/MiWidget.java
@@ -0,0 +1,13 @@
+package com.jdesigner.android.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MiWidget extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+	}
+}
